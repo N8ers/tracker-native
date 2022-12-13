7ad63533572c9020d790e62f6ce2be0b0b8f4140
sheryak_n <nathanraysheryak@gmail.com>
2022-12-13 06:20:09 -0500

    get dummy modal opening/closing

--- screens/Today.js
+++ screens/Today.js
@@ -1,9 +1,32 @@
-import { Text, View } from "react-native"
+import { useState } from "react"
+import { Text, View, StyleSheet, Modal, Button } from "react-native"
 
 export default function Today() {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
   return (
     <View>
-      <Text>I am Today!</Text>
+      <Text>Add Todays Stats!</Text>
+
+      <View style={styles.container}>
+        <Button title="Add" onPress={() => setIsModalVisible(true)} />
+      </View>
+
+      <Modal animationType="slide" visible={isModalVisible}>
+        <View style={styles.modalView}>
+          <Text>hi, record todays weight</Text>
+          <Button title="close" onPress={() => setIsModalVisible(false)} />
+        </View>
+      </Modal>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {},
+  modalView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+})
