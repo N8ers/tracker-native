7d36a92e91b15567dd2a6d39caa1205a918bfbd3
sheryak_n <nathanraysheryak@gmail.com>
2022-12-14 07:24:59 -0500

    make dummy loop for month data

--- screens/Progress.js
+++ screens/Progress.js
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native"
+import { FlatList, SafeAreaView, Text, View } from "react-native"
 
 export default function Progress() {
+  const data = []
+  for (let i = 0; i <= 31; i++) {
+    data.push({ id: i, value: `val - ${i}` })
+  }
+  console.log(data)
+
   return (
     <View>
       <Text>I am stats!</Text>
+
+      <Text>I would be a drop down month selector</Text>
+
+      <SafeAreaView>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <View>
+              <Text>{item.value}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
     </View>
   )
 }
