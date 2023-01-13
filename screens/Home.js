0ff45b7b15d934d5260e0b088ad85090164aca0c
sheryak_n <nathanraysheryak@gmail.com>
2023-01-13 18:24:59 -0500

    remove toast buttons from home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,20 +1,9 @@
-import { Text, View, Button } from "react-native"
-
-import { showSuccessToast, showErrorToast, showInfoToast } from "../util/toasts"
+import { Text, View } from "react-native"
 
 export default function Home() {
   return (
     <View>
       <Text>I am home!</Text>
-      <Button
-        title="Show success"
-        onPress={showSuccessToast({
-          headerText: "Greate success!",
-          subText: "Something succeeded :)",
-        })}
-      />
-      <Button title="Show error" onPress={showErrorToast} />
-      <Button title="Show info" onPress={showInfoToast} />
     </View>
   )
 }
