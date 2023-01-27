850ad91c73db95455d4f92c2822c5f66e92d62a2
sheryak_n <nathanraysheryak@gmail.com>
2023-01-27 07:34:51 -0500

    replace Text with AppText in Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,26 +1,28 @@
-import { Text, View, StyleSheet } from "react-native"
+import { StyleSheet } from "react-native"
+
+import { AppText } from "../components/AppText"
+import { ContentWrapper } from "../components/ContentWrapper"
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Open Source Tracker</Text>
-      <Text>Welcome to OST! </Text>
-      <Text style={styles.blob1}>
-        A way to manage your weight tracking goals, while keeping your data
-        secure. The source code that runs this app is publicly visible (open
-        source) so you can see it doesn't do anything in secret.
-      </Text>
-      <Text style={styles.blob2}>
-        Your data is yours! It's never sold or available to third parties.
-      </Text>
-    </View>
+    <>
+      <ContentWrapper>
+        <AppText style={styles.heading}>Open Source Tracker</AppText>
+        <AppText>Welcome to OST! </AppText>
+        <AppText style={styles.blob1}>
+          A way to manage your weight tracking goals, while keeping your data
+          secure. The source code that runs this app is publicly visible (open
+          source) so you can see it doesn't do anything in secret.
+        </AppText>
+        <AppText style={styles.blob2}>
+          Your data is yours! It's never sold or available to third parties.
+        </AppText>
+      </ContentWrapper>
+    </>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    margin: 20,
-  },
   heading: {
     fontSize: 20,
     marginBottom: 20,
