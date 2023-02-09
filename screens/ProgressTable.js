2a83f65220b69b27ffd5d364d54d6e9d73a3b90d
sheryak_n <nathanraysheryak@gmail.com>
2023-02-09 07:57:11 -0500

    very rough date picker POC

--- screens/ProgressTable.js
+++ screens/ProgressTable.js
@@ -1,11 +1,20 @@
 import { useDispatch, useSelector } from "react-redux"
-import { FlatList, SafeAreaView, Text, View, StyleSheet } from "react-native"
+import {
+  FlatList,
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  Pressable,
+  Button,
+} from "react-native"
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
+import DateTimePicker from "@react-native-community/datetimepicker"
 
 import { useThemes } from "../hooks/useThemes"
 
 import { fetchWeights } from "../store/weights"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function ProgressTable() {
   const weights = useSelector((state) => state.weight.weights)
@@ -19,6 +28,39 @@ export default function ProgressTable() {
     dispatch(fetchWeights())
   }, [])
 
+  const [date, setDate] = useState(new Date())
+  const [mode, setMode] = useState("date")
+  const [show, setShow] = useState(false)
+  const [text, useText] = useState("empty")
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date
+    // setShow(Platform.os == "ios")
+    setDate(currentDate)
+
+    let tempDate = new Date(currentDate)
+    let fDate =
+      tempDate.getDate() +
+      "/" +
+      (tempDate.getMonth() + 1) +
+      "/" +
+      tempDate.getFullYear()
+    let fTime =
+      "Hours: " + tempDate.getHours() + " | Minutes: " + tempDate.getMinutes()
+    setText(fDate + "\n" + fTime)
+    console.log(fDate + " (" + fTime + ")")
+  }
+
+  const showMode = (currnetMode) => {
+    setShow(true)
+    setMode(currnetMode)
+  }
+
+  const openCalendar = (selectedCalendar) => {
+    console.log({ selectedCalendar })
+    // setStartRangeIsOpen(true)
+  }
+
   return (
     <View>
       {!isLoading && (
@@ -29,20 +71,54 @@ export default function ProgressTable() {
               backgroundColor: themes.lightBackground,
               margin: 20,
               padding: 10,
-              flexDirection: "row",
               marginTop: 20,
+              flexDirection: "row",
               justifyContent: "center",
               alignItems: "center",
             }}
           >
-            <Text style={{ color: "white", marginRight: 10 }}>MM/DD/YYY</Text>
-            <MaterialCommunityIcons name="calendar" color="white" size="24" />
+            <Pressable
+              onPress={() => openCalendar("startRange")}
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text style={{ color: "white", marginRight: 10 }}>MM/DD/YYY</Text>
+              <MaterialCommunityIcons name="calendar" color="white" size="24" />
+            </Pressable>
             <Text style={{ marginLeft: 20, marginRight: 20, fontSize: 20 }}>
               |
             </Text>
-            <Text style={{ color: "white", marginRight: 10 }}>MM/DD/YYY</Text>
-            <MaterialCommunityIcons name="calendar" color="white" size="24" />
+            <Pressable
+              onPress={() => openCalendar("endRange")}
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text style={{ color: "white", marginRight: 10 }}>MM/DD/YYY</Text>
+              <MaterialCommunityIcons name="calendar" color="white" size="24" />
+            </Pressable>
           </View>
+
+          <Text>{text}</Text>
+          <Button title="date" onPress={() => showMode("date")} />
+          <Button title="time" onPress={() => showMode("time")} />
+          {show && (
+            <View>
+              <DateTimePicker
+                testId="dateTimePicker"
+                value={date}
+                mode={mode}
+                is24Hour={true}
+                display="default"
+                onChange={onChange}
+              />
+            </View>
+          )}
 
           <SafeAreaView>
             <FlatList
