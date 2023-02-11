a76f8c7ff967ba1a183145e0c2495e0076e17ccc
sheryak_n <nathanraysheryak@gmail.com>
2023-02-11 17:01:49 -0500

    refactor the chart ui and some math

--- screens/ProgressChart.js
+++ screens/ProgressChart.js
@@ -2,30 +2,22 @@ import { useSelector } from "react-redux"
 import { Dimensions, View } from "react-native"
 import { LineChart } from "react-native-chart-kit"
 
+import { useThemes } from "../hooks/useThemes"
+
 import DateRangeSelector from "../components/DateRangeSelector"
 
 const screenWidth = Dimensions.get("window").width
-const chartConfig = {
-  color: (opacity = 0) => `rgba(179, 255, 224, ${opacity})`,
-  strokeWidth: 2, // optional, default 3
-  barPercentage: 0.5,
-  useShadowColorFromDataset: false, // optional
-}
-
-/**
- * TODO
- * [ ] replace a lot of this manual date logic with date-fns
- * [ ] see if there are more chart options/ui stuff that we want to add
- *
- */
 
 export default function ProgressChart() {
   const isLoading = useSelector((state) => state.weight.loading)
   const weightsDesc = useSelector((state) => state.weight.weights)
   const weights = weightsDesc.slice().reverse()
 
+  const themes = useThemes()
+
   const justDates = weights.map((weight) => weight.date)
   const justWeights = weights.map((weight) => weight.weight)
+
   const datesByMonth = {}
   const firstDateOfMonth = {}
 
@@ -47,13 +39,26 @@ export default function ProgressChart() {
 
   const data = {
     labels: justDates,
-    datasets: [
-      {
-        data: justWeights,
-        // color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
-      },
-    ],
-    legend: ["Last 30 Weigh-ins"], // optional
+    datasets: [{ data: justWeights }],
+  }
+
+  const chartConfig = {
+    backgroundColor: themes.background,
+    backgroundGradientFrom: themes.background,
+    backgroundGradientTo: themes.background,
+    decimalPlaces: 1,
+    color: (opacity = 0) => `rgba(179, 255, 224, ${opacity})`,
+    labelColor: (opacity = 0) => `rgba(179, 255, 224, ${opacity})`,
+  }
+
+  const calculateXAxisLabels = (value) => {
+    const month = value.split("-")[1]
+    const monthAsDate = new Date(value)
+    const monthName = monthAsDate.toLocaleString("default", {
+      month: "short",
+      day: "numeric",
+    })
+    return value === firstDateOfMonth[month] ? monthName : ""
   }
 
   return (
@@ -65,18 +70,11 @@ export default function ProgressChart() {
           <LineChart
             data={data}
             width={screenWidth}
-            height={300}
+            height={350}
             chartConfig={chartConfig}
             verticalLabelRotation={0}
-            formatXLabel={(value) => {
-              const month = value.split("-")[1]
-              const monthAsDate = new Date(value)
-              const monthName = monthAsDate.toLocaleString("default", {
-                month: "short",
-                day: "numeric",
-              })
-              return value === firstDateOfMonth[month] ? monthName : ""
-            }}
+            formatXLabel={calculateXAxisLabels}
+            style={{ marginTop: 25 }}
           />
         </View>
       )}
